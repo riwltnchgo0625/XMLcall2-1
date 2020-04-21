847e96d0b947997d4cea3b512df77fb4c820ecf9
riwltnchgo0625 <63505725+riwltnchgo0625@users.noreply.github.com>
2020-04-21 13:03:02 +0900

    Initial commit

---
+++ app/src/main/java/com/example/xmlcall2/MainActivity.java
@@ -0,0 +1,203 @@
+package com.example.xmlcall2;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.URL;
+import java.net.URLEncoder;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText edit;
+    TextView text;
+
+    String Key = "LhJ8ZI%2B5mq2PSlEJf%2FqFKk%2FS9vSYq4tHg0UTKPhddcpzm9cKrFkdIoOVfV%2Bm0HIPkMkIwu0eR1Jag16zh33i%2Fg%3D%3D";
+    String data;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        edit = (EditText)findViewById(R.id.edit);
+        text = (TextView)findViewById(R.id.text);
+
+    }
+
+    public void mOnClick(View v) {
+        switch (v.getId()){
+            case R.id.button:
+
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        data = getXmlData();
+
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                text.setText(data);
+                            }
+                        });
+                    }
+                }).start();
+
+                break;
+
+
+        }
+    }
+
+
+    String getXmlData() {
+
+        StringBuffer buffer = new StringBuffer();
+
+        String str = edit.getText().toString();
+        String location = null;
+        try {
+            location = URLEncoder.encode(str,"UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+        String queryUrl = "http://apis.data.go.kr/6260000/BusanTblFnrstrnStusService/getTblFnrstrnStusInfo?"
+                + "serviceKey="+Key
+                +"&numOfRows=100&pageNo=1";
+
+
+        try {
+            URL url = new URL(queryUrl);
+            InputStream is = url.openStream();
+
+            XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+            XmlPullParser xpp = factory.newPullParser();
+            xpp.setInput(new InputStreamReader(is, "UTF-8"));
+
+            String tag;
+
+            xpp.next();
+            int eventType= xpp.getEventType();
+
+            while( eventType != XmlPullParser.END_DOCUMENT ){
+
+                switch (eventType){
+                    case XmlPullParser.START_DOCUMENT:
+                        buffer.append("파싱시작...\n\n");
+                        break;
+
+                    case XmlPullParser.START_TAG:
+                        tag=xpp.getName();
+
+                        if(tag.equals("item"));
+                        else if(tag.equals("bsnsSector")){
+                            buffer.append("업종 : ");
+                            xpp.next();
+                            buffer.append(xpp.getText());
+                            buffer.append("\n");
+                        }
+                        else if(tag.equals("bsnsCond")){
+                            buffer.append("업태 : ");
+                            xpp.next();
+                            buffer.append(xpp.getText());
+                            buffer.append("\n");
+                        }
+                        else if(tag.equals("bsnsNm")){
+                            buffer.append("업소명 :");
+                            xpp.next();
+                            buffer.append(xpp.getText());//cpId
+                            buffer.append("\n");
+                        }
+                        else if(tag.equals("addrRoad")){
+                            buffer.append("소재지(도로명) :");
+                            xpp.next();
+                            buffer.append(xpp.getText());//cpNm
+                            buffer.append("\n");
+                        }
+                        else if(tag.equals("addrJibun")){
+                            buffer.append("소재지(지번)");
+                            xpp.next();
+                            buffer.append(xpp.getText());//
+                            buffer.append("\n");
+                        }
+                        else if(tag.equals("menu")){
+                            buffer.append("메뉴 :");
+                            xpp.next();
+                            buffer.append(xpp.getText());//
+                            buffer.append("\n");
+                        }
+                        else if(tag.equals("specDate")) {
+                            buffer.append("지정일자 :");
+                            xpp.next();
+                            buffer.append(xpp.getText());//
+                            buffer.append("\n");
+                        }
+                        else if(tag.equals("ovrdDate")) {
+                            buffer.append("재지정일자 :");
+                            xpp.next();
+                            buffer.append(xpp.getText());//
+                            buffer.append("\n");
+                        }
+                        else if(tag.equals("gugun")) {
+                            buffer.append("구군명 :");
+                            xpp.next();
+                            buffer.append(xpp.getText());//
+                            buffer.append("\n");
+                        }
+                        else if(tag.equals("dataDay")) {
+                            buffer.append("데이터기준일자 :");
+                            xpp.next();
+                            buffer.append(xpp.getText());//
+                            buffer.append("\n");
+                        }
+                        else if(tag.equals("lat")) {
+                            buffer.append("위도 :");
+                            xpp.next();
+                            buffer.append(xpp.getText());//
+                            buffer.append("\n");
+                        }
+                        else if(tag.equals("lng")) {
+                            buffer.append("경도 :");
+                            xpp.next();
+                            buffer.append(xpp.getText());//
+                            buffer.append("\n");
+                        }
+                        break;
+                    case  XmlPullParser.TEXT:
+                        break;
+
+                    case XmlPullParser.END_TAG:
+                        tag= xpp.getName();
+                        if(tag.equals("item"))buffer.append("\n");
+                        break;
+
+
+                }
+                eventType= xpp.next();
+
+            }
+
+        }catch(Exception e){
+
+
+        }
+
+        buffer.append("끝");
+        return buffer.toString();
+
+    }
+
+
+}
